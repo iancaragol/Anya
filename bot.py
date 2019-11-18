baa6ba6d3744eb6af4e8bcb4597faeeb90cf869b
Ian Caragol <iancaragol@gmail.com>
2019-11-18 13:11:12 -0700

    Added ad-lib translation!

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@ import discord
 import time
 from discord.ext import commands
 from classes.ad_lib import AdLibber
+from asyncio import futures
 
-bot = commands.Bot(command_prefix='?', description="A translater that tries its best")
-ad_lib = AdLibber()
+bot = commands.Bot(command_prefix='!', description="A translater that tries its best")
+bot.remove_command('help')
+verbose = True
+ad_lib = AdLibber(verbose)
 
 @bot.event
 async def on_ready():
@@ -15,33 +18,98 @@ async def on_ready():
     await bot.change_presence(activity = discord.Game(name="!help"))
 
 @bot.command()
+@commands.is_owner()
+async def shutdown(ctx):
+    await ctx.bot.logout()
+
+#region Hello
+@bot.command()
 async def hello(ctx):
     embed = discord.Embed()
     gif_url = 'https://cdn.discordapp.com/attachments/352281669992185866/500780935638155264/kOXnswR.gif'
     embed.set_image(url=gif_url)
     await ctx.send(embed=embed)
+#endregion
 
+#region Help command
+@bot.command()
+async def help(ctx, *, args = None):
+    help_str = ""
+
+    if (args == None):
+        help_str = '''```diff
+Hello! My name is Anya. I am a terrible translator but I try my best!
+
+The default prefix is !. To learn more about a command type !help [command].
+Like this: !help translate
+
+Commands:
+> hello : Hi!
+> help : Well, you got this far...
+> translate : Ad-lib translation!
+> language: Shows all supported languages and codes
+```'''
+    else:
+        args = args.lower().strip()
+
+        if args == 'translate':
+            help_str = '''```!translate [number of seconds] [language1>language2>etc...]
+
+Translate will record all messages in the channel for the given number of seconds. It then translates the messages as provided to create a fun conversation.
+
+Ex:
+    !translate 10 ko>ja>ru>en
+```'''
+        if args == 'languages':
+            help_str = '''```!languages
+Returns all language codes for use in the translate command.```'''
+
+        else:
+            help_str = "`I couldn't understand your input :(`"
+
+    await ctx.send(help_str)
+#endregion
+
+#region Languages
+@bot.command()
+async def languages(ctx):
+    await ctx.send(ad_lib.get_language_codes())
+#endregion
+
+#region Translate
 @bot.command()
 async def translate(ctx, seconds : int, languages : str):
-    start_msg = await ctx.send("`Reading messages for the next {} seconds!`".format(str(seconds)))
+    check_bool, check_str = ad_lib.check_languages(languages)
+
+    if not check_bool:
+        await ctx.send(check_str)
+        return
+
+    start_msg = await ctx.send("`Reading messages for the next {} seconds! #msg = {}`".format(str(seconds), str(0)))
     t_end = time.time() + seconds
     messages = []
-
+    
     while time.time() < t_end:
-        msg = await bot.wait_for('message')
-        if msg.author.id != 645357754852048906:
-            messages.append((msg.author.name, msg.content))
-            emoji = '\N{THUMBS UP SIGN}'
-            await start_msg.add_reaction(emoji)
+        timeout = t_end - time.time()
+        try:
+            msg = await bot.wait_for('message', timeout=timeout)
+            if msg.author.id != 645357754852048906:
+                messages.append((msg.author.name, msg.content))
+                await start_msg.edit(content="`Reading messages for the next {} seconds! #msg = {}`".format(str(seconds), str(len(messages))))
+        except futures.TimeoutError:
+            break
     
     if len(messages) > 0:
-        await ctx.send("`I read {} messages. Translating now!`".format(len(messages)))
+        if len(messages) != 1:
+            await ctx.send("`I read {} messages. Translating now!`".format(len(messages)))
+        else:
+            await ctx.send("`I read {} message. Translating now!`".format(len(messages)))
         trans_convo = ad_lib.translate_and_format(messages, languages)
         await ctx.send(trans_convo)
     
     else:
         await ctx.send("`No input received in the past {} seconds.`".format(str(seconds)))
-    
+#endregion
     
 
 token = ""
--- classes/ad_lib.py
+++ classes/ad_lib.py
@@ -1,35 +1,59 @@
+import googletrans
+import difflib
 from googletrans import Translator
 
 class AdLibber:
-    def __init__(self):
+    def __init__(self, verbose):
         self.translator = Translator()
+        self.verbose = verbose
 
     def translate_and_format(self, messages, languages):
-        languages_arr = langauges.split('>').lower()
+        languages_arr = languages.lower().split('>')
 
         for l in languages_arr:
-            print("Translating to: " + l)
+            if self.verbose:
+                print("Translating to: " + l)
             trans_msg = self.translate_all(messages, l)
 
-        return self.format(trans_msg, langauges)
+        return self.format(trans_msg, languages)
 
     def translate_all(self, messages, dest_language):
         text_only = [m[1] for m in messages]
         translated_text = self.translator.translate(text_only, dest=dest_language)
 
         for i in range(len(messages)):
-            messages[i] = (messages[i][0], translated_text[i][1])
+            if self.verbose:
+                print(messages[i])
+
+            messages[i] = (messages[i][0], translated_text[i].text)
 
         return messages
 
     def format(self, messages, languages):
-        f_string = "```asciidoc\n" + "Languages: [{}]\n".format(languages.lower())
+        f_string = """```md
+Languages: [{}]\n\n""".format(languages.lower())
 
         for m in messages:
-            add_string = "[{}]: {}\n".format(m[0], m[1])
-            f_string += add_string
+            if len(messages) > 1:
+                add_string = "[{}]: {}\n".format(m[0], m[1])
+                f_string += add_string
+            else:
+                add_string = "{}\n".format(m[1])
+                f_string += add_string
 
         f_string.strip()
         f_string += "```"
 
         return f_string
+
+    def check_languages(self, languages):
+        languages_arr = languages.lower().split('>')
+
+        for l in languages_arr:
+            if l not in googletrans.LANGUAGES.keys():
+                closest_strs = difflib.get_close_matches(l, googletrans.LANGUAGES.keys(), cutoff=0.4)
+                return False, "`I did not recognize the language code {}. Use !languages to see all available language codes.`"
+        return True, ""
+
+    def get_language_codes(self):
+        return "```" + str(googletrans.LANGUAGES).replace(',', '\n')[1:-1] + "```"
